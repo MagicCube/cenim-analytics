235cef0aa0127a3f9e7321df513b8ba0462875fb
Henry Li <henry1943@163.com>
2017-04-17 15:10:12 +0800

    Add save_json() and save_data()

--- cenim/utils/__init__.py
+++ cenim/utils/__init__.py
@@ -16,3 +16,14 @@ def load_json(path):
 def load_data(name):
     path = ''.join(['data/', name, '.json'])
     return load_json(path)
+
+
+def save_json(dict, path):
+    f = codecs.open(path, 'w', 'utf-8')
+    json.dump(dict, f, ensure_ascii=False)
+    f.close()
+
+
+def save_data(data, name):
+    path = ''.join(['data/', name, '.json'])
+    save_json(data, path)

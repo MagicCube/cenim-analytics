d574cfcff72c92673e6439c448ea21fcc71cfb3d
Henry Li <henry1943@163.com>
2017-04-24 15:10:08 +0800

    Remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='cenim',
-    version='0.1.0',
-    packages=['cenim'],
-    entry_points={
-        'console_scripts': ['cenim-train = cenim.runnables.train:run']
-    })
